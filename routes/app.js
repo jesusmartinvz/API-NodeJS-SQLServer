7797eb9f54fc978e8b76f74465e028e8d60455e8
jesusmartinvz <jesus_martim@hotmail.com>
2022-11-17 20:30:30 -0500

    status

--- routes/app.js
+++ routes/app.js
@@ -41,7 +41,21 @@ router.get('/ecommerce', (req, res) => {
 router.get('/ecommerce/:factura', (req,res) =>{
     const {factura } = req.params
     const sql = `SELECT * FROM cab_venta Where NumFactura = ${factura}`;
-    /* #swagger.responses[200] = {
+      
+        connection.query(sql, (error, result) => {
+        
+            if(error) throw error;
+
+            if(result.length > 0) {
+                res.json(result);
+                res.status(200).send(result);
+
+            }else{
+                res.status(404).send('Boleta no encontrada');
+                res.send('No existe la boleta');
+            }
+        });
+        /* #swagger.responses[200] = {
           description: "Operacion exitosa",
           content: {
             "application/json": {
@@ -51,61 +65,9 @@ router.get('/ecommerce/:factura', (req,res) =>{
             }
           }
       }
-    */    
-        connection.query(sql, (error, result) => {
-        
-            if(error) throw error;
-
-            if(result.length > 0) {
-                //res.json(result);
-                res.send(result);
-                res.status(200).send(result);
-
-            }else{
-                res.status(404).send('Boleta no encontrada');
-                res.send('No existe la boleta');
-            }
-        });
+    */  
         
 });
-/*
-
-router.post('/ecommerce/add', (req, res) =>{
-    const sql = 'INSERT INTO cab_venta SET ?';
-    const ventaObj = {
-        NumFactura: req.body.NumFactura,
-        Total_Fac: req.body.Total_Fac,
-        Fecha_Fac: req.body.Fecha_Fac,
-        IdUsuario: req.body.IdUsuario,
-        id_estado: req.body.id_estado
-    };
-    connection.query(sql, ventaObj, error => {
-        if(error) throw error;
-        res.send('Venta Generada');
-    });
-});
-
-router.put('/ecommerce/update/:id', (req, res) =>{
-    const {id } = req.params;
-    const {id_estado} = req.body;
-    const sql = `UPDATE cab_venta set id_estado = '${id_estado}' WHERE Id_Venta = ${id}`;
-
-    connection.query(sql, error => {
-        if(error) throw error;
-        res.send('Factura modificada');
-    });
-})
-
-router.delete('/ecommerce/delete/:id', (req, res) =>{
-    const {id } = req.params;
-    const sql = `DELETE FROM cab_venta WHERE Id_Venta = ${id}`;
-    connection.query(sql, error => {
-        if(error) throw error;
-        res.send('FACTURA ELIMINADA');
-    })
-
-})
-*/
 
 
 //createPOOL
